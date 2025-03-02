60f34cab05cbda8f3d4e927e56c949879ac8c2ce
VaderNgo <vaderngo@gmail.com>
2025-03-02 21:44:43 +0700

    Add Lab_01 project

---
+++ Homeworks/Lab_01/src/basic.js
@@ -0,0 +1,9 @@
+export function setPixel(imageData, x, y, pointRgba) {
+    if (x < imageData.width && y < imageData.height) {
+        var index = (x + y * imageData.width) << 2;
+        imageData.data[index + 0] = pointRgba[0]; // Red
+        imageData.data[index + 1] = pointRgba[1]; // Green
+        imageData.data[index + 2] = pointRgba[2]; // Blue
+        imageData.data[index + 3] = pointRgba[3]; // Alpha (opacity)
+    }
+}
